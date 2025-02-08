73b50d5939a7e96ff1c46ae638f2392fe5b4969a
Lucas ANDRES <lucas.andres@epitech.eu>
2025-02-08 13:01:25 +0100

    feat: add burnable tree who burn in present and are destroy in futur

---
+++ Assets/Script/Burn_Tree.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Burn_Tree : MonoBehaviour
+{
+    private TagHandle fireBall;
+    public GameObject fire;
+    public GameObject Burned_tree;
+    public GameObject Burnable_tree;
+    public void OnEnable()
+    {
+        fireBall = TagHandle.GetExistingTag("FireBall");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(fireBall))
+        {
+            fire.SetActive(true);
+            Burned_tree.SetActive(true);
+            Burnable_tree.SetActive(false);
+            Destroy(other.gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
