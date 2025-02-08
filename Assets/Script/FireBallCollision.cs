ed26e404e0ee4e58d7bd6a60e826fff3cd197f3c
Mambokara <corentin.bergaentzle@epitech.eu>
2025-02-08 02:25:32 +0100

    feat : Add generic function to destroy fireBalls when collisioning with an object

---
+++ Assets/Script/FireBallCollision.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FireBallCollision : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
